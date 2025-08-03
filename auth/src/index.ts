c0e426de7dd2be93ca1b3d6276787309aa2dda93
khaliltkhalil <khalil.t.khalil@live.com>
2025-08-03 14:06:32 -0500

    add starting log info

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 import { app } from "./app";
 const start = async () => {
+  console.log("Starting auth service...");
   try {
     if (!process.env.JWT_KEY) {
       throw new Error("no JWT_KEY");
