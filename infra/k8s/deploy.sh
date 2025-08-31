92b80856a735e9bb99774348e790fb60ac000379
khaliltkhalil <khalil.t.khalil@live.com>
2025-08-30 22:13:53 -0500

    update manifest

--- infra/k8s/deploy.sh
+++ infra/k8s/deploy.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-skaffold dev --module kafka &
-sleep 5
-echo "Starting create-topics module"
-skaffold dev --module create-topics &
-#in the future add a check to see if the create-topic-job is complete before starting the microservices
-sleep 40
-skaffold dev --module microservices
+echo "deplying kafka"
+kubectl apply -f kafka &
+sleep 10
+echo "deploying create-topics job"
+kubectl apply -f create-topics &
+#in the future add a check to see if the create-topic-job is complete before starting the microservices.
+sleep 60
+echo "deploying microservices"
+kubectl apply -f microservices
+echo "deploying ingress"
+kubectl apply -f k8s-ingress-prod
